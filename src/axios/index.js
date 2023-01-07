6a2ab81c45510004aa59d8627d7977bf46ebac39
makysheva.aliya <makysheva.aliya@gmail.com>
2023-01-08 04:47:24 +0600

    Изменение baseURL

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-    baseURL: 'http://localhost:5656',
+    baseURL: 'https://react-blog-diplom.vercel.app',
     withCredentials: false,
     headers: {
         "Access-Control-Allow-Origin": "*",
